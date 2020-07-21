6dda5e544dd370eee65a59b58827d3211ba26bb8
Augusto Zvoboter <zvoboter@gmail.com>
2020-07-21 20:25:02 -0300

    Criação do layout da tela principal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <main>
+        <h1>Gerador de CNPJ</h1>
+        <form>
+          <div className="content-left">
+            <div className="input-block">
+              <input name="cnpj" id="cnpj" placeholder="00.000.000/0001-01" contentEditable="false"></input>
+            </div>
+            <div className="recent-list">
+              <ul>
+                <li>00.000.000/0001-01</li>
+                <li>00.000.000/0001-01</li>
+                <li>00.000.000/0001-01</li>
+                <li>00.000.000/0001-01</li>
+              </ul>
+            </div>
+          </div>
+          <div className="content-right">
+            <button type="submit">Gerar</button>
+          </div>
+        </form>
+
+
+      </main>
+      <footer></footer>
+    </>
   );
 }
 
