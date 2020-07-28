c820f7f2fb1ed617f7b4571228983bb0032c5578
Augusto Zvoboter <zvoboter@gmail.com>
2020-07-28 20:05:02 -0300

    Adicionado biblioteca de icones

--- src/components/CNPJForm/index.js
+++ src/components/CNPJForm/index.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import { createCnpj } from '../../services/cnpj';
+import { FaClipboard, FaRegClipboard } from 'react-icons/fa';
 
 import './style.css';
 
 function CNPJForm() {
     const [cnpj, setCnpj] = useState('');
+    const [recentCnpjs, setRecentCnpjs] = useState([]);
 
     const handleCNPJ = (e) => {
         e.preventDefault();
 
+        cnpj && setRecentCnpjs([cnpj, ...recentCnpjs]);
         setCnpj(createCnpj());
+    }
+
+    const handleCopyCnpj = (cnpjValue, clean) => {
+        let input = document.createElement('input');
+        input.value = clean ? cnpjValue.replace(/\D/g, '') : cnpjValue;
+
+        document.body.appendChild(input);
+        input.focus();
+        input.select();
+        input.setSelectionRange(0, 99999);
+
+        console.log(input.value)
+        document.execCommand("copy");
+
+        input.remove();
     }
 
     return (
@@ -21,14 +39,31 @@ function CNPJForm() {
                         value={cnpj}
                         readOnly={true}
                         placeholder="00.000.000/0001-01"
-                        contentEditable="false" />
+                        contentEditable="false"
+                    />
+
+                    <FaClipboard className="icon-input" onClick={(e) => handleCopyCnpj(cnpj, false)}></FaClipboard>
+                    <FaRegClipboard className="icon-input" onClick={(e) => handleCopyCnpj(cnpj, true)}></FaRegClipboard>
                 </div>
 
-                <div className="recent-list">
-                    <ul>
-                        <li>{'00.000.000/0001-01'}</li>
-                    </ul>
-                </div>
+                {recentCnpjs.length > 0 &&
+                    <>
+                        <h3>CNPJ's recentes</h3>
+                        <div className="recent-list" >
+                            <ul>
+                                {recentCnpjs.map((cnpjItem) => (
+                                    <li key={cnpjItem}>
+                                        <div>
+                                            <span>{cnpjItem}</span>
+                                            <FaClipboard className="icon-input" onClick={() => handleCopyCnpj(cnpjItem, false)} />
+                                            <FaRegClipboard className="icon-input" onClick={() => handleCopyCnpj(cnpjItem, true)} />
+                                        </div>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </>
+                }
             </div>
             <div className="content-right">
                 <button type="submit">Gerar</button>
